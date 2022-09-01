a2c926abca221d43935eca614d84a3bfccf43d81
KadonWills <kapolw@gmail.com>
2022-09-01 16:26:32 +0100

    Updated hardware attributes ui

--- src/components/HardwarePerformanceCard.tsx
+++ src/components/HardwarePerformanceCard.tsx
@@ -8,7 +8,7 @@ type HardwarePerformanceCardProps = {
 
 const HardwarePerformanceCard:FunctionComponent<HardwarePerformanceCardProps> = ({title, icon, attributes}) => {
   return (
-    <div className='flex flex-col space-y-4 items-center w-full h-max' >
+    <div className='flex flex-col space-y-4 items-center w-full h-max border-l-2 border-opacity-10 border-primary p-2' >
         <h3 className='text-base font-bold font-sans uppercase '>{title}</h3>
         <div className='text-5xl'>
             {icon}
@@ -16,14 +16,14 @@ const HardwarePerformanceCard:FunctionComponent<HardwarePerformanceCardProps> = 
         <div className='flex flex-col space-y-3 w-full  items-center'>
             {
                 attributes.map((attr) => (
-                    <div className='flex flex-row flex-1 space-x-1 w-full text-xs font-medium font-mono  mx-auto'> 
+                    <div className='flex flex-row flex-1 space-x-1 w-11/12 text-xs font-medium font-mono  mx-auto'> 
                         <span className='w-[30%] whitespace-nowrap mr-auto'>
                             {attr.title}
                         </span>
                         <div className='w-[33%] h-[1.1rem] cursor-not-allowed text-right px-3 mx-auto border border-[#bbb]  align-middle bg-[#ffffff58]'>
                             {attr.value}
                         </div>
-                        <span className='w-[30%]'>
+                        <span className='w-[10%]'>
                             {attr.unit}
                         </span>
                     </div>
