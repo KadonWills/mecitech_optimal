72b1a9ca4655b21fddd136d43859c53a41353d9e
KadonWills <kapolw@gmail.com>
2022-09-03 04:31:15 +0100

    Feature: Store preferred theme in localStorage and auto-update theme on first load

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -1,15 +1,25 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 const ThemeSwitcher = () => {
 
     const [theme, setTheme] = useState<string>('')
 
+    useEffect(() => {
+        if(localStorage.getItem("theme") == "dark") {
+            document.body.classList.add("dark")
+            setTheme("dark")
+        }
+    }, [])
+
+
     const handleSwitch = () => {
         if (theme == "") {
             setTheme("dark")
             document.body.classList.add("dark")
+            localStorage.setItem("theme", "dark")
         } else {
             document.body.classList.remove("dark")
+            localStorage.setItem("theme", "")
             setTheme('')
         }
     }
